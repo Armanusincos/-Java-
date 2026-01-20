56e61689eba03db35bef9f1c1517d5a8879e7423
unknown <armanisuynov@gmail.com>
2026-01-20 14:26:54 +0500

    Инфо о Студенте

---
+++ Lab 1/Student.java
@@ -0,0 +1,16 @@
+public class Student {
+
+
+public static void main(String[] args) {
+    String fullName = "Суюнов Арман";
+    String group = "ИС-22-21";
+    String specialty = "Информационные Системы";
+
+    System.out.println("Информация о студенте:");
+    System.out.println("ФИО: " + fullName);
+    System.out.println("Группа: " + group);
+    System.out.println("Специальность: " + specialty);
+}
+
+
+}
